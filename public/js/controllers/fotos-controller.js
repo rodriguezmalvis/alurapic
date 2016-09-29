ab7ef49295ec230d9d77151acb479044fd6f3a8c
rrm <you@example.com>
2016-09-29 20:54:48 -0300

    capitulo 4 acho

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,22 @@
+angular.module('alurapic').controller('FotosController', function($scope, $http){
+
+	$scope.fotos = [];
+	$scope.filtro = '';
+
+	var promise = $http.get("v1/fotos")
+	.success(function(fotos){
+		$scope.fotos = fotos;
+	}).error(function(erro){
+		console.log(erro);
+	});
+
+	/*
+	var promise = $http.get("v1/fotos");
+	promise.then(function(retorno){
+		$scope.fotos = retorno.data;
+	}).catch(function(error){
+		console.log(erro);
+	});
+	*/
+
+});
